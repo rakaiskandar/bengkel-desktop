9c09ecb273e80a2890479d6baf94e6e825007a1c
rakaiskandar <iskandarraka4@gmail.com>
2025-05-19 10:34:53 +0700

    fix: database connectivity

---
+++ src/services/Database.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author musa
+ */
+public class Database {
+    private final Connection connection;
+
+    public Database() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_mahasiswa", "root", "");
+        } catch (SQLException e) {
+            throw new RuntimeException("Error saat menghubungkan database: " + e.getMessage(), e);
+        } 
+    }
+    
+    public ResultSet selectQuery(String sql, Object... params) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error saat menjalankan select query: " + e.getMessage(), e);
+        }
+    }
+    
+    public int executeUpdate(String sql, Object... params) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error saat menjalankan update query: " + e.getMessage(), e);
+        }
+    }
+    
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Eror saat menutup koneksi database: " + e.getMessage(), e);
+        }
+    }
+}
